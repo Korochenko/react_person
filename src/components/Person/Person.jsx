ca01174d094cb149a8b48e445d1ca1d5402a8a29
Kostia <kostantinkorocenko301@gmail.com>
2025-06-29 21:12:36 +0300

    test

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
+
+      {person.isMarried === true ? (
+        <p className="Person__partner"> {person.partnerName} is my wife</p>
+      ) : (
+        <p>I am not married</p>
+      )}
+    </section>
+  </div>
+);
